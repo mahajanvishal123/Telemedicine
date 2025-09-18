3c4486e2d0c3de0b1158451504032f8a01e36275
sh-vani <shivanikush2002@gmail.com>
2025-09-18 04:16:37 -0700

    changes

--- src/Components/AdminDashboard/Dashboard.jsx
+++ src/Components/AdminDashboard/Dashboard.jsx
@@ -149,26 +149,7 @@ const Dashboard = () => {
         </div>
 
         {/* Users */}
-        <div className="col-xl-3 col-md-6 mb-4">
-          
-            <div className="card border-left-warning shadow h-100 py-2">
-              <div className="card-body">
-                <div className="row no-gutters align-items-center">
-                  <div className="col mr-2">
-                    <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                      Total Users</div>
-                    <div className="h5 mb-0 font-weight-bold text-gray-800">
-                      {dashboardData.totalUsers}
-                    </div>
-                  </div>
-                  <div className="col-auto">
-                    <i className="fas fa-users fa-2x text-warning"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-         
-        </div>
+      
         
         {/* Total Users Card */}
         
